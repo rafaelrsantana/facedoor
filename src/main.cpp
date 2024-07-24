fb6c9cb3c48c175c6f4f5a9aa263ae8f975d1e94
Rafael <rafaelsantanaps@gmail.com>
2024-07-24 15:25:45 -0300

    teste

---
+++ src/main.cpp
@@ -0,0 +1,131 @@
+
+
+// Piscar o flash:
+/*
+#include <Arduino.h>
+
+#define PIN_FLASHLIGHT 4
+#define NUM_BLINKS 5
+
+int blinkCounter = 0;
+
+void setup() {
+    pinMode(PIN_FLASHLIGHT, OUTPUT);
+}
+
+void loop() {
+    if (blinkCounter < NUM_BLINKS) {
+        digitalWrite(PIN_FLASHLIGHT, HIGH);
+        delay(1000);
+        digitalWrite(PIN_FLASHLIGHT, LOW);
+        delay(1000);
+        blinkCounter++;
+    } else {
+        digitalWrite(PIN_FLASHLIGHT, LOW); // Garante que o flash está desligado
+        while (true) {
+            // Para o loop principal, evitando mais piscadas
+        }
+    }
+}
+// Término piscar o flash
+*/
+
+
+// Live Camera:
+
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <Arduino.h>
+
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+#include "camera_pins.h"
+
+const char *ssid = "iPhone";
+const char *password = "123456789";
+
+void startCameraServer();
+void setupLedFlash(int pin);
+
+void setup() {
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println();
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.frame_size = FRAMESIZE_UXGA;
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+
+    if (config.pixel_format == PIXFORMAT_JPEG) {
+        if (psramFound()) {
+            config.jpeg_quality = 10;
+            config.fb_count = 2;
+            config.grab_mode = CAMERA_GRAB_LATEST;
+        } else {
+            config.frame_size = FRAMESIZE_SVGA;
+            config.fb_location = CAMERA_FB_IN_DRAM;
+        }
+    } else {
+        config.frame_size = FRAMESIZE_240X240;
+    }
+
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    if (s->id.PID == OV3660_PID) {
+        s->set_vflip(s, 1);
+        s->set_brightness(s, 1);
+        s->set_saturation(s, -2);
+    }
+    s->set_framesize(s, FRAMESIZE_QVGA);
+
+    WiFi.begin(ssid, password);
+    WiFi.setSleep(false);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    startCameraServer();
+
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+}
+
+void loop() {
+    delay(10000);
+}
+
+
+// Término Live Camera
